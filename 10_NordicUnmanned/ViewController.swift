3efd2d338f65db2d6afb876306b4c1d9f23b2dbf
marielaulie <54407324+marielaulie@users.noreply.github.com>
2021-07-02 12:30:48 +0200

    Opprettet app

---
+++ 10_NordicUnmanned/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  10_NordicUnmanned
+//
+//  Created by Mariel Aulie Hinderaker on 02/07/2021.
+//  Copyright © 2021 Mariel Aulie Hinderaker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
